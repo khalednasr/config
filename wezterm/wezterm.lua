cadf14ab5f805d2034b39795b7ad4db6fccaf6a8
Khaled Nasr <k.nasr92@gmail.com>
2025-03-09 01:12:21 +0100

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'catppuccin-mocha'
+-- config.color_scheme = 'kanagawabones'
 
 -- config.font = wezterm.font '0xProto Nerd Font'
 
 config.default_prog = {'wsl.exe', '--shell-type', 'login', '--cd', '~', 'zellij'}
 
 config.colors = {
+	background = '#16161d',
 	cursor_bg = 'grey',
 }
 
@@ -22,5 +23,6 @@ config.window_decorations = "RESIZE|TITLE"
 
 config.max_fps = 120
 config.prefer_egl = true
+config.window_close_confirmation = 'NeverPrompt'
 -- and finally, return the configuration to wezterm
 return config
