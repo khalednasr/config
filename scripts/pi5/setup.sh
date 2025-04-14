9be4062afb0e52696ea115f250282e029e373aa6
Khaled Nasr <k.nasr92@gmail.com>
2025-04-14 02:11:48 +0200

    update

--- scripts/pi5/setup.sh
+++ scripts/pi5/setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env fish
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
+sudo apt update
+sudo apt install wezterm
+
 sudo apt remove xsel xclip
 sudo apt install wl-clipboard
 
