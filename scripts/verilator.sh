8a15729402c6e29dcf1b4e9dd69469cabb1086f6
Khaled Nasr <k.nasr92@gmail.com>
2025-04-22 17:04:50 +0200

    update

---
+++ scripts/verilator.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+# Prerequisites:
+sudo apt-get install git help2man perl python3 make autoconf g++ flex bison ccache
+sudo apt-get install libgoogle-perftools-dev numactl perl-doc
+sudo apt-get install libfl2  # Ubuntu only (ignore if gives error)
+sudo apt-get install libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
+
+git clone https://github.com/verilator/verilator ~/verilator   # Only first time
+
+cd ~/verilator
+git checkout master      # Use development branch (e.g. recent bug fixes)
+
+autoconf         # Create ./configure script
+./configure      # Configure and create Makefile
+make -j `nproc`  # Build Verilator itself (if error, try just 'make')
+sudo make install
+cd -
