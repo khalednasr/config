d00dda6a75c40279087ae72f07b9c3ab8e553cab
Khaled Nasr <k.nasr92@gmail.com>
2025-06-29 23:58:55 +0200

    remove svlangserver ensure_installed in mason

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,12 +6,6 @@ wk.add({
   { "L", "<cmd>bn<cr>", desc = "Next buffer", mode = "n" },
   { "<leader>x", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
 
-  -- flash jumps
-  -- { "f",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-  -- { "F",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-  -- { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-  -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
- 
    -- top pickers & explorer
   { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
   { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
